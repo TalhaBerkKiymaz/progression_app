4a17f33e645005eb10840df7a651119067464130
Ahmet Erdogan <329188@student.mboutrecht.nl>
2021-02-03 14:21:02 +0100

    connect_db en index toegevoegd

---
+++ connect_db.php
@@ -0,0 +1,9 @@
+<?php
+define("SERVERNAME", "localhost");
+define("USERNAME", "power");
+define("PASSWORD", "secret");
+define("DATABASENAME", "progressionapp");
+
+$conn = mysqli_connect(SERVERNAME, USERNAME, PASSWORD, DATABASENAME);
+
+?>
