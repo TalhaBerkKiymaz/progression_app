944686ddf9cc44cd2a2b75c239695c3cfe534f2b
Talha Berk Kiymaz <326753@student.mboutrecht.nl>
2021-02-03 15:56:02 +0100

    home v1.01

--- home.php
+++ home.php
@@ -5,19 +5,29 @@
  */
 include("./connect_db.php");
 
-$sql = "SELECT  c.class_name, cc.lessons, c.education from class_course as cc, class as c where c.class_name = 'am2a' and c.class_name = cc.class_name";
+$sql = "SELECT  c.class_name, cc.lessons, c.education from class_course as cc, class as c GROUP BY cc.lessons";
 
 // SELECT  c.class_name, cc.lessons, c.education from class_course as cc, class as c GROUP BY cc.lessons
+// SELECT  c.class_name, cc.lessons, c.education from class_course as cc, class as c where c.class_name = 'am2a' and c.class_name = cc.class_name"
 
 $result = mysqli_query($conn, $sql);
 
-print_r($sql);
+$show = "<div class='row'>";
+while ($record = mysqli_fetch_assoc($result)) {
+    $show .= "<div class='col-3'>
+                <div class= 'card mb-2' style='width: 18rem;'>
+                    <div class='card-body'>
+                        <h5 class='card-title'>{$record['lessons']}</h5>
+                        <h6 class='card-subtitle mb-2 text-muted'>{$record['education']}</h6>
+                        <p class='card-text'>{$record['class_name']}</p>
+                        <a href='#' class='card-link'>Card link</a>
+                        <a href='#' class='card-link'>Another link</a>
+                    </div>
+                </div>
+            </div>";
 
+    var_dump($record);
+}
 
-
-
-
-
-
-
+echo $show;
 ?>
