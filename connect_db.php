2973c8ea3e7725730dfc90bf6af621e62ebd3726
Talha Berk Kiymaz <326753@student.mboutrecht.nl>
2021-02-05 13:27:24 +0100

    connect_db aangepast

--- connect_db.php
+++ connect_db.php
@@ -1,8 +1,8 @@
 <?php
 define("SERVERNAME", "localhost");
-define("USERNAME", "power");
-define("PASSWORD", "secret");
-define("DATABASENAME", "progressionapp");
+define("USERNAME", "id16090476_talha");
+define("PASSWORD", "disA95GmbnsGQ7~W");
+define("DATABASENAME", "id16090476_progapp");
 
 $conn = mysqli_connect(SERVERNAME, USERNAME, PASSWORD, DATABASENAME);
 
