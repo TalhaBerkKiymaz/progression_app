4d3e3fa19d2add18ac4978dc8307500daf73933f
Ahmet <leaforewer.net@gmail.com>
2021-02-13 20:17:21 +0100

    register.sql toegevoegd

---
+++ db/register.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: 127.0.0.1:3306
+-- Üretim Zamanı: 13 Şub 2021, 19:16:43
+-- Sunucu sürümü: 5.7.31
+-- PHP Sürümü: 7.3.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `progressionapp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `register`
+--
+
+DROP TABLE IF EXISTS `register`;
+CREATE TABLE IF NOT EXISTS `register` (
+  `user_id` int(10) NOT NULL AUTO_INCREMENT,
+  `email` varchar(55) NOT NULL,
+  `password` varchar(55) NOT NULL,
+  `userrole` enum('admin','director','teacher','student') NOT NULL,
+  `activated` bit(1) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
