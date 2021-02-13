3e992b89eac5f39742ca7eca4d991e39368f3109
Ahmet <leaforewer.net@gmail.com>
2021-02-13 20:03:45 +0100

    Stater template

---
+++ banner.php
@@ -0,0 +1,2 @@
+<?php session_start(); session_gc(); ?>
+<img src="./img/vegetablejuice.png" alt="vegetablejuice" class="img-fluid w-100">
--- index.php
+++ index.php
@@ -1,44 +1,57 @@
-<?php ob_start(); ?>
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PMA</title>
-    <link rel="stylesheet" href="./css/style.css">
+    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <!-- FONTS -->
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
-    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
 
+    <link rel="stylesheet" href="./css/style.css">
 
-</head>
-
-<body>
-
-    <main class="container">
-
-        <!-- content --></br>
-
-        <div class="col-12">
-            <div class="row">
-
-                <?php include("./content.php"); ?>
-            </div>
+    <link rel="shortcut icon" href="./img/icons/favicon.ico" type="image/x-icon">
+    <title>Vegetable Juice</title>
+  </head>
+  <body>
+    <main>
+      <section class="container-fluid">
+        <div class="row">
+          <div class="col-12 px-0">
+            <?php include("./banner.php"); ?>
+          </div>
         </div>
+      </section>
+      <section class="container-fluid">
+        <div class="row">
+          <div class="col-12 px-0">
+            <?php include("./navbar.php"); ?>
+          </div>
         </div>
-
-
-
+      </section>
+      <section class="container-fluid">
+        <div class="row">
+          <div class="col-12">
+            <?php include("./content.php"); ?>
+          </div>
+        </div>
+      </section>
+      <section class="container-fluid fixed-bottom">
+        <div class="row">
+          <div class="col-12 px-0">
+            <?php include("./footer.php"); ?>
+          </div>
+        </div>
+      </section>
     </main>
 
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>    
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <script src="./js/app.js"></script>
-</body>
-
+  </body>
 </html>
