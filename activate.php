9ab7e39889bec00c8728c757b62e483e6a6da09f
Talha Berk Kiymaz <326753@student.mboutrecht.nl>
2021-02-14 21:57:13 +0100

    Login pagina en register pagina layout verbeterd

--- activate.php
+++ activate.php
@@ -4,27 +4,21 @@
  }
 ?>
 
-<div class="container mt-5">
-  <div class="row">
-    <div class="col-12 col-sm-6">
+
       <form action="./index.php?content=activate_script" method="post">
-        <div class="form-group">
+        <div class="form-group login-script">
+          <h1>Activeren</h1>
           <label for="inputPassword">Kies een nieuw wachtwoord:</label>
-          <input name="password" type="password" class="form-control" id="inputPassword" aria-describedby="passwordHelp" autofocus>
-          <small id="passwordHelp" class="form-text text-muted">Kies een veilig wachtwoord..</small>
-        </div>
-        <div class="form-group">
+          <input name="password" type="password" placeholder="Kies een wachtwoord" class="form-control" id="inputPassword" aria-describedby="passwordHelp" autofocus>
+          <small id="passwordHelp" class="form-text">Kies een veilig wachtwoord..</small>
+
           <label for="inputPasswordCheck">Type het wachtwoord opnieuw:</label>
-          <input name="passwordCheck" type="password" class="form-control" id="inputPasswordCheck" aria-describedby="passwordHelpCheck">
-          <small id="passwordHelpCheck" class="form-text text-muted">Ter controle voert u nogmaals uw wachtwoord in...</small>
+          <input name="passwordCheck" type="password" placeholder="Type opnieuw" class="form-control" id="inputPasswordCheck" aria-describedby="passwordHelpCheck">
+          <small id="passwordHelpCheck" class="form-text">Ter controle voert u nogmaals uw wachtwoord in...</small>
+
+          <button type="submit" class="btn btn-success btn-lg btn-block">Activeer</button>
         </div>
         <input type="hidden" name="id" value="<?php echo $_GET["id"]; ?>">
         <input type="hidden" name="pwh" value="<?php echo $_GET["pwh"]; ?>">
-        <button type="submit" class="btn btn-success btn-lg btn-block">Activeer</button>
+        
       </form>
-    </div>
-    <div class="col-12 col-sm-6">
-      <img src="./img/perzik.jpg" alt="perzik" class="w-75 mx-auto d-block">
-    </div>
-  </div>
-</div>
--- login.php
+++ login.php
@@ -1,25 +1,20 @@
-<div class="container mt-5">
-  <div class="row">
-    <div class="col-12 col-sm-6">
 
-      <form action="./index.php?content=login_script" method="post">
-        <div class="form-group">
-          <label for="inputEmail">Vul hier uw e-mailadres in:</label>
-          <input name="email" type="email" class="form-control" id="inputEmail" aria-describedby="emailHelp" autofocus>
-          <small id="emailHelp" class="form-text text-muted">Uw privacy is gegarandeerd...</small>
-        </div>
-        <div class="form-group">
-          <label for="inputPassword">Vul hier wachtwoord in:</label>
-          <input name="password" type="password" class="form-control" id="inputPassword" aria-describedby="passwordHelp">
-          <small id="passwordHelp" class="form-text text-muted">Pas op voor meekijkers tijdens het invoeren...</small>
-        </div>
+
+    <form action="./index.php?content=login_script" method="post">
+
+        <div class="form-group login-picture">
+        <h1>Sign in</h1>
+            <label for="inputEmail">Vul hier uw e-mailadres in:</label>
+            <input name="email" type="email" placeholder="@mboutrecht.nl of @student.mboutrecht.nl" class="form-control" id="inputEmail" aria-describedby="emailHelp" autofocus>
+            <small id="emailHelp" class="form-text ">Uw privacy is gegarandeerd...</small>
+        
+        
+            <label for="inputPassword">Vul hier wachtwoord in:</label>
+            <input name="password" type="password" placeholder="Wachtwoord" class="form-control" id="inputPassword" aria-describedby="passwordHelp">
+            <small id="passwordHelp" class="form-text">Pas op voor meekijkers tijdens het invoeren...</small>
+        
         
         <button type="submit" class="btn btn-danger btn-lg btn-block mt-4">Log in</button>
-      </form>
+        </div>
+    </form>
 
-    </div>
-    <div class="col-12 col-sm-6">
-      <img src="./img/passievrucht.jpg" alt="kersen" class="w-75 mx-auto d-block">
-    </div>
-  </div>
-</div>
